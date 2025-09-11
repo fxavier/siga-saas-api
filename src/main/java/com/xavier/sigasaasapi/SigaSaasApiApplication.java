2512bd908b32d8f466f3a96253d631f4fdfd288a
Xavier Francisco <xavierfrancisco353@gmail.com>
2025-09-11 14:17:54 +0100

    first commit

---
+++ src/main/java/com/xavier/sigasaasapi/SigaSaasApiApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.sigasaasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigaSaasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigaSaasApiApplication.class, args);
+    }
+
+}
